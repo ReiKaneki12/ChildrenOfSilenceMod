bfe6942e83ae1215dd53ab9b04b04cbf3218e074
ReiKaneki12 <PC@DESKTOP-TE5IED9>
2024-08-30 14:59:55 -0300

    coisas - arthur normal

--- src/main/java/net/mcreator/childrenofsilence/procedures/WhileFireBallIsFlyingProcedure.java
+++ src/main/java/net/mcreator/childrenofsilence/procedures/WhileFireBallIsFlyingProcedure.java
@@ -3,18 +3,17 @@ package net.mcreator.childrenofsilence.procedures;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.entity.Entity;
 
-import net.mcreator.childrenofsilence.network.ChildrenOfSilenceModVariables;
+import net.mcreator.childrenofsilence.ChildrenOfSilenceMod;
 
 public class WhileFireBallIsFlyingProcedure {
 	public static void execute(LevelAccessor world, Entity entity, Entity immediatesourceentity) {
 		if (entity == null || immediatesourceentity == null)
 			return;
-		if (ChildrenOfSilenceModVariables.MapVariables.get(world).WorldJustLoaded) {
+		immediatesourceentity.setNoGravity(true);
+		assert Boolean.TRUE; //#dbg:WhileFireBallIsFlying:NoGravity
+		ChildrenOfSilenceMod.queueServerWork(200, () -> {
 			if (!entity.level().isClientSide())
 				entity.discard();
-		} else if (!ChildrenOfSilenceModVariables.MapVariables.get(world).WorldJustLoaded) {
-			immediatesourceentity.setNoGravity(true);
-			assert Boolean.TRUE; //#dbg:WhileFireBallIsFlying:NoGravity
-		}
+		});
 	}
 }
