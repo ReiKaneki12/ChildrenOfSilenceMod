88d07790e5964b3a7518db581a5639b90066451d
ReiKaneki12 <geomt@Arthur>
2024-08-30 19:28:42 -0300

    no processo pra consertar a porra da bola de fogo - arthur narguileiro

--- src/main/java/net/mcreator/childrenofsilence/procedures/WhileFireBallIsFlyingProcedure.java
+++ src/main/java/net/mcreator/childrenofsilence/procedures/WhileFireBallIsFlyingProcedure.java
@@ -3,17 +3,16 @@ package net.mcreator.childrenofsilence.procedures;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.entity.Entity;
 
-import net.mcreator.childrenofsilence.ChildrenOfSilenceMod;
+import net.mcreator.childrenofsilence.network.ChildrenOfSilenceModVariables;
 
 public class WhileFireBallIsFlyingProcedure {
 	public static void execute(LevelAccessor world, Entity entity, Entity immediatesourceentity) {
 		if (entity == null || immediatesourceentity == null)
 			return;
 		immediatesourceentity.setNoGravity(true);
-		assert Boolean.TRUE; //#dbg:WhileFireBallIsFlying:NoGravity
-		ChildrenOfSilenceMod.queueServerWork(200, () -> {
+		if (ChildrenOfSilenceModVariables.MapVariables.get(world).WorldJustLoaded) {
 			if (!entity.level().isClientSide())
 				entity.discard();
-		});
+		}
 	}
 }
